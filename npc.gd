bce83b530b1678af176c4c4ebc6d12f83216b624
Petunia-stack <tpmoloto123@gmail.com>
2026-07-31 18:29:55 +0200

    Delete npc.gd

--- npc.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-func _on_area_2d_body_entered(_body):
-	get_node("../DialogueText").visible = true
-
-
-func _on_area_2d_body_exited(_body):
-	get_node("../DialogueText").visible = false
